c598f3bf55dbc1cf8bf472adc9827f220a483926
Ray Huang <rayhuang.cj@gmail.com>
2020-03-15 00:33:57 -0700

    Completed Template + Started home

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  HashRouter,
+  Switch,
+  Redirect,
+  Route  
+} from "react-router-dom";import './App.css';
+
+import TopNav from "./nav/top.jsx"
+// Connect pages
+import Home from "./pages/home.js"
+import Professional from "./pages/professional.js"
+import Business from "./pages/business.js"
+
+//import SignUp from "./account/signin.js"
+//import SignIn from "./account/sinup.js"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <HashRouter name="app" path="/" handler={App} basename={process.env.PUBLIC_URL}>
+        <TopNav/>
+          <Switch>
+            <Route path = "/pages/home.js" exact component={Home} />
+            <Route path = "./pages/professional.js" exact component={Professional} />
+            <Route path = "/pages/business.js" exact component={Business} />
+           </Switch>
+        <Redirect from="/" exact to="/pages/home.js" />
+      </HashRouter>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/pages/professional.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import "./home.css";
+
+class Professional extends Component { 
+    render() {
+      return (
+          <React.Fragment>
+            
+          </React.Fragment>
+      );
+    }
+  }
+  
+  export default Professional;
