1d3a8a330780212193c6636e9664f63d77313640
David Deng <daviddeng8@s-169-232-238-131.resnet.ucla.edu>
2020-03-22 18:26:11 -0700

    added basic firebase functionality for storing account information

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,28 +1,59 @@
 import React, { Component } from 'react';
-/*import fire from './src/config/Fire.js'*/
+import fire from './config/fire.js'
 
 import "./login.css";
 
 class Login extends Component { 
+    constructor(props) {
+      super(props);
+      this.login = this.login.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+      this.signup = this.signup.bind(this);
+      this.state = {
+        email:'',
+        password:''
+      }
+    }
+
+    login(e) {
+      e.preventDefault();
+      fire.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then(
+        (u)=>{}).catch((error) => { 
+        console.log(error);
+      });
+    }
+
+    signup(e) {
+      e.preventDefault();
+      fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch(
+        (error) => { 
+        console.log(error);
+      });
+    }
+
+    handleChange(e) {
+      this.setState({ [e.target.name]: e.target.value });
+    }
+
     render() {
       return (
           <React.Fragment>
             <form>
               <div class = "form-group">
                 <label for="exampleInputEmail1">Email address</label>
-                <input type = "email" name = "email" 
-                class = "form-control" id = "exampleInputEmail1" 
-                aria-describedby = "emailHelp" placeholder = "Enter email" />
+                <input value={this.state.email} onChange={this.handleChange} 
+                type="email" name="email" class="form-control" id="exampleInputEmail1" 
+                aria-describedby="emailHelp" placeholder="Enter email" />
               </div>
 
               <div class = "form-group">
                 <label for="exampleInputPassword1">Password</label>
-                <input type = "password" name = "password" 
-                class = "form-control" id = "exampleInputPassword1" 
-                placeholder = "Password" />
+                <input value={this.state.password} onChange={this.handleChange}
+                type="password" name="password" class="form-control" 
+                id="exampleInputPassword1" placeholder="Password" />
               </div>
 
-              <button>Create Account</button>
+              <button onClick={this.signup}>Create Account</button>
 
             </form>
           </React.Fragment>
