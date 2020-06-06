7603fbaa8197aee5f29a648090d310c959eec9d2
Daniel Covelli <covellidanny@gmail.com>
2020-06-06 16:50:46 -0700

    Added dist folder for deployment

---
+++ dist/build/precache-manifest.ab1d937569b80f7ad12f6ab39e49868f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee620711fc15308a7dab4e326e3678e",
+    "url": "/brandsocialite/index.html"
+  },
+  {
+    "revision": "215f2a094c5e4f47ed44",
+    "url": "/brandsocialite/static/css/2.c2f27783.chunk.css"
+  },
+  {
+    "revision": "1bf183811a40d8b6a663",
+    "url": "/brandsocialite/static/css/main.240f9fef.chunk.css"
+  },
+  {
+    "revision": "215f2a094c5e4f47ed44",
+    "url": "/brandsocialite/static/js/2.07b818ae.chunk.js"
+  },
+  {
+    "revision": "494ff15427fc00bc6d2909d429ff11b9",
+    "url": "/brandsocialite/static/js/2.07b818ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf183811a40d8b6a663",
+    "url": "/brandsocialite/static/js/main.7a3ddf4d.chunk.js"
+  },
+  {
+    "revision": "874e2fc2a0b42e78c13e",
+    "url": "/brandsocialite/static/js/runtime-main.beda9e1c.js"
+  },
+  {
+    "revision": "4290ea77f18e51028fc4eb0b77136e7e",
+    "url": "/brandsocialite/static/media/BebasNeue-Regular.4290ea77.ttf"
+  },
+  {
+    "revision": "9ab0544da4ed08ba58b22d235baf57ce",
+    "url": "/brandsocialite/static/media/BebasNeue-Regular.9ab0544d.woff2"
+  },
+  {
+    "revision": "afeaf6797b6286b72b613ababc563807",
+    "url": "/brandsocialite/static/media/BebasNeue-Regular.afeaf679.woff"
+  },
+  {
+    "revision": "c83438f8c95b4913dac1121ceaf46419",
+    "url": "/brandsocialite/static/media/BebasNeue-Regular.c83438f8.otf"
+  },
+  {
+    "revision": "cca5d8a15e7179bdc9978728314d50b1",
+    "url": "/brandsocialite/static/media/BebasNeue-Regular.cca5d8a1.eot"
+  },
+  {
+    "revision": "0c1f680e63723a0b59d9a8369b80a7e9",
+    "url": "/brandsocialite/static/media/bartender.0c1f680e.jpg"
+  },
+  {
+    "revision": "d10b985321259c83a875fd335442c433",
+    "url": "/brandsocialite/static/media/brand_page.d10b9853.jpg"
+  },
+  {
+    "revision": "9f4ac367287c2c1a41115c21cdeda734",
+    "url": "/brandsocialite/static/media/event.9f4ac367.jpg"
+  },
+  {
+    "revision": "513f3735beb404488d84298f0fc49857",
+    "url": "/brandsocialite/static/media/flat_logo.513f3735.PNG"
+  },
+  {
+    "revision": "3aef078fa1d6009a9024fd655cc5260b",
+    "url": "/brandsocialite/static/media/photographer_old.3aef078f.jpg"
+  },
+  {
+    "revision": "b2cd9468a015e3c16d02fe9d6567bb18",
+    "url": "/brandsocialite/static/media/sommelier.b2cd9468.jpg"
+  },
+  {
+    "revision": "659e1e463ba0369749d2b12fa826256e",
+    "url": "/brandsocialite/static/media/talent.659e1e46.jpg"
+  },
+  {
+    "revision": "4142843463586cc3b1145147fc8852f3",
+    "url": "/brandsocialite/static/media/transparent_logo.41428434.png"
+  }
+]);
