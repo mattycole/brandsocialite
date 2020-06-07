ead4a453a396f16dc83fa8f6325bbf52a9816e99
Daniel Covelli <covellidanny@gmail.com>
2020-06-06 19:03:05 -0700

    Updated brand and talent page, added Join Now button

--- src/components/pages/brands/brands.js
+++ src/components/pages/brands/brands.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
-import { Button, Container} from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 
-import "./brands.css";
+import './brands.css';
 
-import BrandImg from "../../../assets/website_imgs/brand_page.jpg";
+import BrandImg from '../../../assets/website_imgs/brand_page.jpg';
 
-class Brand extends Component { 
-    render() {
-      return (
-
-        <div className="container">
-          <div className="header">
-            <h1 class="home-title">
-              <span>Your incomparable brand deserves</span>
-              <span>an incomparable event staffing solution</span>
-            </h1>
-          </div>
-          <div className>
-            <h1 className="body_title">
-              Our Promise
-            </h1>
-          </div>
-          <div className="text">
-            <p>
-            We are committed to sourcing the most qualified staff for your brand event. Our technology platform only retains talent that is of the highest industry standard for quality, experience, and dependability. We prioritize attention to detail and deepen our alignment with the brands who entrust to us.            </p>
-          </div>
+class Brand extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className>
+          <h1 className='body_title'>Connecting Talent to Brands</h1>
         </div>
-
-      );
-    }
+        <div className='text'>
+          <p className='body_text'>
+            We are committed to sourcing the most qualified staff for your brand
+            event. Our technology platform only retains talent that is of the
+            highest industry standard for quality, experience, and
+            dependability. We prioritize attention to detail and deepen our
+            alignment with the brands who entrust to us.{' '}
+          </p>
+        </div>
+        <div className='button'>
+          <a href='#/create' class='brk-btn mobile-frame'>
+            Join Now
+          </a>
+        </div>
+      </div>
+    );
   }
-  
-  export default Brand;
+}
+
+export default Brand;
--- src/components/pages/talent/talent.js
+++ src/components/pages/talent/talent.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-import { Button, Container} from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 
-import "./talent.css";
+import './talent.css';
 
-class Talent extends Component { 
-    render() {
-      return (
-            <Container>
-              <div className="container">
-                <div className="talent_header">
-                  <h1 className="body_title">
-                    Our Promise
-                  </h1>
-                </div>
-                <div className="text">
-                  <p>
-                  We are committed to match the talent on our platform with the highest paid event staffing opportunities in their local area. Our technology platform is an all-in-one solution for everything from job notifications to event updates and payments.
-                  </p>
-                </div>
-              </div>
-            </Container>
-          
-      );
-    }
+class Talent extends Component {
+  render() {
+    return (
+      <Container>
+        <div className='container'>
+          <div className='talent_header'>
+            <h1 className='body_title'>Find Work at Top Brands</h1>
+          </div>
+          <div className='text'>
+            <p className='body_text'>
+              We are committed to match the talent on our platform with the
+              highest paid event staffing opportunities in their local area. Our
+              technology platform is an all-in-one solution for everything from
+              job notifications to event updates and payments.
+            </p>
+          </div>
+          <div className='button'>
+            <a href='#/create' class='brk-btn mobile-frame'>
+              Join Now
+            </a>
+          </div>
+        </div>
+      </Container>
+    );
   }
-  
-  export default Talent;
+}
+
+export default Talent;
